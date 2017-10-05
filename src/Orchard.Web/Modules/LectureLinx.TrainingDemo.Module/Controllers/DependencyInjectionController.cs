5feeaeb31d32b95fd394622e98d1c6e31ea00674
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2017-10-05 18:08:14 +0200

    Module development basics

---
+++ src/Orchard.Web/Modules/LectureLinx.TrainingDemo.Module/Controllers/DependencyInjectionController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Orchard.Localization;
+using Orchard.Themes;
+using Orchard.UI.Notify;
+
+namespace LectureLinx.TrainingDemo.Module.Controllers
+{
+    [Themed]
+    public class DependencyInjectionController : Controller
+    {
+        private readonly INotifier _notifier;
+
+        public Localizer T { get; set; }
+
+
+        public DependencyInjectionController(INotifier notifier)
+        {
+            _notifier = notifier;
+
+            T = NullLocalizer.Instance;
+        }
+
+
+        public ActionResult NotificationDemo()
+        {
+            _notifier.Information(T("Hello world!"));
+
+            return View();
+        }
+    }
+}
