381c0eb1cc204faff62ae623b6a6ebda5866a83b
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2017-11-14 18:56:54 +0100

    Custom Token and Workflows Activity providers

---
+++ src/Orchard.Web/Modules/LectureLinx.TrainingDemo.Module/Activities/WarningActivity.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard.Forms.Services;
+using Orchard.Localization;
+using Orchard.UI.Notify;
+using Orchard.Workflows.Models;
+using Orchard.Workflows.Services;
+
+namespace LectureLinx.TrainingDemo.Module.Activities
+{
+    public class WarningActivity : Task
+    {
+        private readonly INotifier _notifier;
+
+        public Localizer T { get; set; }
+
+
+        public WarningActivity(INotifier notifier)
+        {
+            _notifier = notifier;
+
+            T = NullLocalizer.Instance;
+        }
+
+
+        public override LocalizedString Category { get { return T("Notification"); } }
+
+        public override LocalizedString Description { get { return T("Displays a warning."); } }
+
+        public override string Name { get { return "Warning"; } }
+
+        public override string Form { get { return "WarningActivity"; } }
+
+        public override IEnumerable<LocalizedString> Execute(WorkflowContext workflowContext, ActivityContext activityContext)
+        {
+            _notifier.Warning(T(activityContext.GetState<string>("Message")));
+
+            yield return T("Done");
+
+            //if (true)
+            //{
+            //    yield return T("Accept");
+            //}
+            //else
+            //{
+            //    yield return T("Reject");
+            //}
+        }
+
+        public override IEnumerable<LocalizedString> GetPossibleOutcomes(WorkflowContext workflowContext, ActivityContext activityContext)
+        {
+            yield return T("Done");
+
+            //return new[]
+            //{
+            //    T("Accept"),
+            //    T("Reject")
+            //};
+        }
+    }
+
+
+    public class WarningActivityForm : IFormProvider
+    {
+        public Localizer T { get; set; }
+
+
+        public WarningActivityForm()
+        {
+            T = NullLocalizer.Instance;
+        }
+
+
+        public void Describe(DescribeContext context)
+        {
+            context.Form("WarningActivity", shapeFactory =>
+            {
+                var shape = (dynamic)shapeFactory;
+                return shape.Form(
+                        Id: "WarningActivity",
+                        _Message: shape.Textbox(
+                            Id: "Message",
+                            Name: "Message",
+                            Title: T("Message"),
+                            Description: T("The warning message to display."),
+                            Classes: new[] { "tokenized", "text", "medium" }));
+            });
+        }
+    }
+}
---
+++ src/Orchard.Web/Modules/LectureLinx.TrainingDemo.Module/Tokens/UtcNowTokens.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard.Localization;
+using Orchard.Services;
+using Orchard.Tokens;
+
+namespace LectureLinx.TrainingDemo.Module.Tokens
+{
+    public class UtcNowTokens : ITokenProvider
+    {
+        private readonly IClock _clock;
+
+        public Localizer T { get; set; }
+
+
+        public UtcNowTokens(IClock clock)
+        {
+            _clock = clock;
+
+            T = NullLocalizer.Instance;
+        }
+
+
+        public void Describe(DescribeContext context)
+        {
+            // {UtcNow.Default} {UtcNow.Short}
+
+            // LectureLinx.TrainingDemo.Module.Tokens.UtcNowTokens "UTC Date/Time Now" -> "Jelenlegi UTC dátum/idő"
+
+            context.For("UtcNow", T("UTC Date/Time Now"), T("Current UTC Date/Time."))
+                .Token("Default", T("Default"), T("The current Date/Time in UTC, with default formatting."), "Text");
+        }
+
+        public void Evaluate(EvaluateContext context)
+        {
+            context.For("UtcNow", () => _clock.UtcNow)
+                .Token("Default", dateTime => dateTime.ToString())
+                .Chain("Default", "Text", dateTime => dateTime.ToString());
+        }
+    }
+}
