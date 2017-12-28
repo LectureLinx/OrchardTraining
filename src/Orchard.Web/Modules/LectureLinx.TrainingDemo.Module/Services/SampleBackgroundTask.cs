326cc0bd1a336ed50c1469ff344ee8b3fb046615
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2017-12-28 17:00:16 +0100

    Adding background task sample and search and indexing configured

---
+++ src/Orchard.Web/Modules/LectureLinx.TrainingDemo.Module/Services/SampleBackgroundTask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard;
+using Orchard.Logging;
+using Orchard.Tasks;
+
+namespace LectureLinx.TrainingDemo.Module.Services
+{
+    public class SampleBackgroundTask : Component, IBackgroundTask
+    {
+        public void Sweep()
+        {
+            Logger.Error("This is just a test.");
+        }
+    }
+}
